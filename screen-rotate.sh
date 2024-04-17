fbf7c7cf320ffafaa83c31509736cf04b50ccec3
LearnTestDev <proxyacc123456@gmail.com>
2024-04-17 03:44:58 +0300

    Minor fixes.

--- screen-rotate.sh
+++ screen-rotate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 screen="eDP" 
+default_screen_orientation="right"
 
 screen_info=$(xrandr --query | grep "$screen")
 
-if [[ "$screen_info" == *"right ("* ]]; then
+if [[ "$screen_info" == *"$default_screen_orientation ("* ]]; then
     xrandr --output eDP --rotate normal
 else
     xrandr --output eDP --rotate right
